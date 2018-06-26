87ccd2d3bb170d472ef636c09b54e335e53aaf4c
Frank Murphy <fmurphy@instructure.com>
2018-06-26 18:41:50 -0400

    More maps

--- alive.js
+++ alive.js
@@ -4,28 +4,135 @@ counties = Array.from(counties.children)
 let states = {}
 let state = (el) => el.id.split(', ')[1]
 
-fetch('./all.json').then((response) => {
-    return response.json()
-}).then((data) => {
-    let stride = 1
-    let starts = [null].concat(_.range(13, 20, stride))
-    let ends = _.range(13, 20, stride).concat([null])
-    let bins = _.zip(starts, ends).map((a) => ({ start: a[0], end: a[1] }))
-    let inBin = (bin) => (n) =>
-        (!_.isNil(n)) && (n >= (bin.start || 0) && n < (bin.end || 100))
+let gradient = (start, end) => (x) => {
+    let premix = (k) => Math.floor(start[k] + (x * (end[k] - start[k])))
+    let mix = (k) => _.clamp(premix(k), 0, 255)
+    return 'rgb(' + [mix('r'), mix('g'), mix('b')].join(', ') + ')'
+}
 
+let absoluteColors = ({ bins, min, max }) => {
     let colorCount = bins.length - 2
     let gradient = (ix) => Math.floor(ix / colorCount * 255)
     let colorRange = _.range(colorCount).map((ix) => (
         'rgb(' + (255 - gradient(ix)) + ', 255, 0)'
     ))
-    let colors = ['rgb(255, 196, 0)']
-                 .concat(colorRange)
-                 .concat(['rgb(0, 255, 255)'])
+    return ['rgb(255, 196, 0)']
+            .concat(colorRange)
+            .concat(['rgb(0, 255, 255)'])
+}
 
+let lossGradient = gradient(
+    { r: 255, g: 230, b: 230 },
+    { r: 255, g: 32, b: 32 }
+)
+let gainGradient = gradient(
+    { r: 230, g: 255, b: 230 },
+    { r: 32, g: 255, b: 32 }
+)
+let diffColors = ({ bins, min, max }) => {
+    return bins.map((bin) => {
+        let start = _.defaultTo(bin.start, min)
+        let end = _.defaultTo(bin.end, max)
+        if (start < 0) {
+            let b = Math.min(0, max)
+            return lossGradient((start - b) / (min - b))
+        } else {
+            let a = Math.max(0, min)
+            return gainGradient((end - a) / (max - a))
+        }
+    })
+}
+
+let inequalityGradient = gradient(
+    { r: 255, g: 192, b: 128 },
+    { r: 255, g: 128, b: 32 }
+)
+let inequalityColors = ({ bins, min, max }) => {
+    return bins.map((bin) => {
+        let start = _.defaultTo(bin.start, min)
+        let end = _.defaultTo(bin.end, max)
+        let midpoint = (start + end) / 2
+        return inequalityGradient((midpoint - min) / (max - min))
+    })
+}
+
+let charts = {
+    current: {
+        min: 13,
+        max: 20,
+        coloring: absoluteColors,
+        variable: 'Median Wage',
+        f: _.identity
+    },
+    minWageDiff: {
+        min: -15,
+        max: -6,
+        coloring: diffColors,
+        variable: 'Min - Median Wage Gap',
+        f: (wage) => 7.25 - wage
+    },
+    inequality: {
+        min: 11,
+        max: 16,
+        coloring: inequalityColors,
+        variable: 'Min - Median Wage Gap',
+        f: (wage) => wage - 7.25 > 10 ? wage - 7.25 : null
+    },
+    damage: {
+        min: -3,
+        max: 0,
+        coloring: diffColors,
+        variable: '15 / hr - Median Wage Gap',
+        f: (wage) => wage < 15 ? wage - 15 : null
+    },
+    minToHalf: {
+        min: 0,
+        max: 4,
+        coloring: diffColors,
+        variable: 'Wage Difference',
+        f: (wage) => Math.max(0, (wage * 0.5) - 7.25)
+    },
+    half: {
+        min: 7,
+        max: 13,
+        coloring: absoluteColors,
+        variable: 'New Minimum Wage',
+        f: (wage) => Math.max(wage * 0.5, 7.25)
+    },
+    noChange: {
+        min: 7,
+        max: 8,
+        coloring: ({ bins }) => bins.map(_.constant('rgb(128, 128, 128)')),
+        variable: 'Same Wage',
+        f: (wage) => (wage * 0.5) > 7.25 ? null : 7.25
+    },
+    diff15: {
+        min: -2,
+        max: 5,
+        coloring: diffColors,
+        variable: 'Wage Difference',
+        f: (wage) => wage - 15
+    }
+}
+
+let config = charts.current
+
+fetch('./all.json').then((response) => {
+    return response.json()
+}).then((data) => {
+    let stride = 1
+    let { coloring, f, min, max, variable } = config
+    let starts = [null].concat(_.range(min, max, stride))
+    let ends = _.range(min, max, stride).concat([null])
+    let bins = _.zip(starts, ends).map((a) => ({ start: a[0], end: a[1] }))
+    let alt = (v, d) => v === null ? d : v
+    let inBin = (bin) => (n) =>
+        (!_.isNil(n)) && (n >= alt(bin.start, -100) && n < alt(bin.end, 100))
+
+    let colors = coloring({ bins, min, max })
     let color = (amount) => _.zip(bins, colors)
                              .filter((pair) => inBin(pair[0])(amount))
-                             .map((pair) => pair[1])[0] || '#f0f'
+                             .map((pair) => pair[1])[0] || '#fff'
 
     let amounts = []
     counties.forEach((child) => {
@@ -41,7 +148,10 @@ fetch('./all.json').then((response) => {
         let median = () => document.getElementById('median')
 
         let wage = _.get(county, '0.wage')
-        if (!_.isNil(wage)) { amounts.push(wage) }
+        if (!_.isNil(wage)) {
+            wage = f(parseFloat(wage))
+            amounts.push(parseFloat(wage))
+        }
         let base = 'fill: ' + color(wage)
 
         if (county.length === 0) {
@@ -60,7 +170,7 @@ fetch('./all.json').then((response) => {
                 if (_.isNil(wage)) {
                     return 'No Data'
                 } else {
-                    return 'Median Wage: ' + _.trim(wage)
+                    return variable + ': ' + wage.toFixed(2)
                 }
             }
             median().innerHTML = wageText()
@@ -91,7 +201,10 @@ fetch('./all.json').then((response) => {
         let label = () => {
             let start = bin.start
             let end = bin.end
-            let fmt = (v) => v.toFixed(0)
+            let fmt = (v) => {
+                let x = (v < 0 ? -v : v).toFixed(0)
+                return v < 0 ? '(' + x + ')' : x
+            }
             if (start === null) {
                 return '< $' + fmt(end)
             } else if (end === null) {
